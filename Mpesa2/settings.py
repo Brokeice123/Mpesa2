51871c58f4c4ab254f4515695896ef548e92e6f4
Brokeice123 <keithmarkomen@gmail.com>
2023-05-17 13:17:31 +0300

    vercel

--- Mpesa2/settings.py
+++ Mpesa2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l+b_ho5u$sz1m6vampfib&-vg$-4_tobzqt$il*@1h#lbi&qm1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
